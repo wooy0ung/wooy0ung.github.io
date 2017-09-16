4ee986ff9fb7d9ba7c2671bdadf2f5686784294e
wooy0ung <wooy0ung@outlook.com>
2017-09-16 08:00:31 +0800

    wooy0ung

--- assets/js/verify.js
+++ assets/js/verify.js
@@ -1,5 +1,5 @@
 (function(){
-    if (CryptoJS.MD5(prompt('请输入文章密码','')).toString() !== 'c3da3d2af39eff6584444fbae78136a7'){
+    if (CryptoJS.MD5(CryptoJS.MD5(CryptoJS.MD5(prompt('请输入文章密码','')).toString()).toString()).toString() !== '376e433440a8723b4137eff4510b1fb4'){
         window.location.href = "../../../../";
         alert('密码错误！');
     }
